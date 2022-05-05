3933a6cd7df7e71b6160c6722700a8953391d94b
WeihongPan <1377758695@qq.com>
2022-05-05 19:51:07 +0800

    add evaluate code for multiple model

--- config.py
+++ config.py
@@ -1,12 +1,28 @@
 import configargparse
 
-def get_model_args():
+def get_life_args():
 
     parser = configargparse.ArgParser(config_file_parser_class=configargparse.YAMLConfigFileParser)
     parser.add_argument('--iters', type=int, default=12)
     parser.add_argument('--mixed_precision', type=bool, default=True)
     parser.add_argument('--small', action='store_true', help='use small model')
-    parser.add_argument('--model', type=str, default="./model/pretrain.pth", help="choose the trained model")
+    parser.add_argument('--model', type=str, default="./model/pretrain.pth", help="choose the trained model")    
+
+    args = parser.parse_known_args()[0]
+    return args
+
+def get_raft_args():
+    #parser = argparse.ArgumentParser()
+    parser = configargparse.ArgParser(config_file_parser_class=configargparse.YAMLConfigFileParser)
+    parser.add_argument("--mixed_precision", type=bool, default=True, required=False)
+    parser.add_argument("--small", type=str, default=False, required=False)
+    parser.add_argument("--iters", type=int, default=12, required=False)
+    parser.add_argument("--dim_corr", type=int, default=192, required=False)
+    parser.add_argument("--dim_corr_coarse", type=int, default=64, required=False)
+    parser.add_argument("--dim_corr_all", type=int, default=256, required=False)    
+    #parser.add_argument("--model", type=str, default="./model/RAFT_twins_bs12_ep20.pth", required=False)
+    parser.add_argument("--model", type=str, default="./model/RAFT_latest.pth", required=False)
+
 
     args = parser.parse_known_args()[0]
     return args
